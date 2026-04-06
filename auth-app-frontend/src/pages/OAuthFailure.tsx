cfad5eb1d6956a1868276b8da1931a65e3097068
AK1715 <ns031077@gmail.com>
2026-04-06 10:28:38 +0530

    oauth failure land page

---
+++ auth-app-frontend/src/pages/OAuthFailure.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OAuthFailure = () => {
+  return (
+    <div className='p-10 flex justify-center items-center'>
+        <h1>login failed!!</h1>
+    </div>
+  )
+}
+
+export default OAuthFailure
